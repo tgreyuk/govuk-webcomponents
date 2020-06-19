548ee57c0be01155df65e127b714fd57ec901f04
tgreyuk <tgreyuk@gmail.com>
2020-06-20 00:04:08 +0100

    Added character  count component

---
+++ components/govuk-character-count/README.md
@@ -0,0 +1,41 @@
+# Character Count
+
+See: https://design-system.service.gov.uk/components/character-count/
+
+## Usage
+
+**Js**
+
+```javascript
+import 'govuk-webcomponents/components/govuk-character-count.component.js';
+```
+
+**Markup**
+
+```markup
+<govuk-character-count></govuk-character-count>
+```
+
+
+
+## &lt;govuk-character-count&gt;
+
+
+### Properties
+
+| Property  |  Type     |
+|-----------|-----------|
+| `maxwords` | `number` |
+| `maxlength` | `number` |
+| `rows` | `number` |
+| `id` | `string` |
+| `name` | `string` |
+| `label` | `string` |
+| `hint` | `string` |
+| `message` | `string` |
+| `pageLevel` | `boolean` |
+
+
+
+
+
---
+++ components/govuk-character-count/govuk-character-count.component.js
@@ -0,0 +1,45 @@
+import componentStyles from './govuk-character-count.styles';
+import { CharacterCount } from './govuk-character-count.script';
+
+import { TextAreaComponent } from '../govuk-textarea/govuk-textarea.component';
+
+export class CharacterCountComponent extends TextAreaComponent {
+  static get properties() {
+    return {
+      maxlength: { type: Number },
+      maxwords: { type: Number },
+    };
+  }
+
+  static get styles() {
+    return [super.styles, componentStyles];
+  }
+
+  constructor() {
+    super();
+    this.maxlength = 200;
+    this.message = `You can enter up to ${this.maxlength} characters`;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  firstUpdated() {
+    const el = this.shadowRoot.firstElementChild;
+    const textarea = this.shadowRoot.querySelector('textarea');
+    textarea.setAttribute('class', 'govuk-textarea govuk-js-character-count');
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'govuk-character-count');
+    wrapper.setAttribute('data-module', 'govuk-character-count');
+    wrapper.setAttribute('data-maxlength', this.maxlength);
+    if (this.maxwords) {
+      wrapper.setAttribute('data-maxwords', this.maxwords);
+    }
+    el.parentNode.insertBefore(wrapper, el);
+    wrapper.appendChild(el);
+    new CharacterCount(wrapper).init();
+  }
+}
+
+customElements.define('govuk-character-count', CharacterCountComponent);
